4faa6c4884e49aa321cfd497bf78b7ac6b8d6315
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2024-02-07 05:13:16 +0000

    feat: generate token for users

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -8,5 +8,6 @@ from apps.user import views
 app_name = 'user'
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create')
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token')
 ]
